3268eedb8aa16d6581ba45910e652b43437ddb9c
Lucas Harada <lucasyharada@gmail.com>
2023-01-31 03:24:24 -0500

    add colors from style-guide.md

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'hsl(25, 97%, 53%)',
+        white: 'hsl(0, 0%, 100%)',
+        lightgrey: 'hsl(217, 12%, 63%)',
+        mediumgrey: 'hsl(216, 12%, 54%)',
+        darkblue: 'hsl(213, 19%, 18%)',
+        verydarkblue: 'hsl(216, 12%, 8%)'
+      },
+      fontSize: {
+        default: '15px'
+      }
+    },
   },
   plugins: [],
 };
