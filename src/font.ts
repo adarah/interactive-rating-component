87ecb703d90bc653904cf08b6451a7a03a0a2b54
Lucas Harada <lucasyharada@gmail.com>
2023-01-31 03:22:26 -0500

    use overpass font

---
+++ src/font.ts
@@ -0,0 +1,3 @@
+import { Overpass } from '@next/font/google'
+
+export const overpass = Overpass({ weight: ['400', '700'], subsets: ['latin'] })
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import { overpass } from "../font";
 
 import "../styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <div className={overpass.className}>
+      <Component {...pageProps} />
+    </div>
+  )
 };
 
 export default MyApp;
